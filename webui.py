1be429832f42345f0d59c3653de3c3cee06c0869
Dennis Aleynikov <daleynikov@gmail.com>
2023-03-11 23:56:41 -0800

    import the necessary stuff

--- webui.py
+++ webui.py
@@ -1,6 +1,14 @@
 try:
     import gradio as gr
-    import tinygrad
+    import os
+    import sys, argparse, math, platform
+    import numpy as np
+    from tqdm import tqdm
+    np.set_printoptions(linewidth=200)
+    from typing import Optional
+
+    from tinygrad.helpers import getenv, DEBUG
+    from tinygrad.lazy import Device
 
 except ModuleNotFoundError as moduleNotFound:
     print(f'Found that `gradio` is not installed, try installing it first')
