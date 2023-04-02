5ba71333600aede5ec107151d329ce331f1c06a8
Dennis Aleynikov <daleynikov@gmail.com>
2023-04-01 21:41:55 -0700

    gallery by llama

---
+++ lib/gallery.dart
@@ -0,0 +1,36 @@
+import FlutterStateNotificationsPlugin 
+  
+class GalleryApp extends StatefulWidget {
+    const GalleryApp({Key? key}) : super(key);
+    
+    @override
+    _GalleryApp createState() => new _GalleryApp();
+}
+
+class _GalleryApp extends State<GalleryApp> with FlutterStateNotificationsPlugin 
+{  
+    final List<String> galleryItems = [
+        "Image1.jpg",
+        "Image2.jpg" ,
+     ];
+      
+      void showNotification(BuildContext context, String message) {
+          Notificatons().showSnackBarWithAction(context, 
+                    'Gallery', 
+                'Show full screen photos with left and right buttons');  
+        }
+        
+      @override
+       Widget build  (){    
+           return new Scaffold(
+               appbar: new AppBar(),
+              body:new Container(width:200, height:150) { 
+                    child : new ListView.builder(  
+                itemCount: galleryItems.length ,
+            itemBuilder: (BuildContext context, int index){      
+               return new Card(
+             child: new FadeInImage(imageSource: "images/${galleryItems[index]}"), 
+           ) ;     }});   
+        showNotification(context, 'Got it!');  
+      }
+}
